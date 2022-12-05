76e13ff705f464c62b6755bc88bdafb3632693f1
Gribanov Evgeniy <gribanov_eo@block5.tech>
2022-12-05 17:19:26 +0300

    feat: Добавил модель новости

--- django_sample/news/models.py
+++ django_sample/news/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=256)
+
+    description = models.TextField()
+
+    last_edited = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "articles"
