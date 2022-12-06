6e23702b8cd6bc82755ecf4f23502b0e3b2b9a6f
Gribanov Evgeniy <gribanov_eo@block5.tech>
2022-12-06 16:15:01 +0300

    feat: Добавил разрабатываемый модуль поля в модель в админке

--- django_sample/news/models.py
+++ django_sample/news/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
+# Только для тестирования и разработки!
+import sys
+sys.path.append(".")
+
+from src.django_editorjs_field import EditorJSField
+
 
 class Article(models.Model):
     title = models.CharField(max_length=256)
 
-    description = models.TextField()
+    description = EditorJSField()
 
     last_edited = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
