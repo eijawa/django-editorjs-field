6b931e879ff209f7543e0f0cfeb665db983b965a
Evgeniy Gribanov <evgeniy.therabbit@gmail.com>
2022-12-21 11:33:33 +0300

    feat: Добавил новый аттрибут в EditorJSField для получения словаря используемых плагинов. Добавил поддержку стандартного плагина, который идёт по-умолчанию - Paragraph

--- django_editorjs_field/field.py
+++ django_editorjs_field/field.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from functools import cached_property
+
 from .tool import EditorJSTool as Tool
 from .widget import EditorJSWidget
 
@@ -16,8 +18,14 @@ class EditorJSField(models.JSONField):
 
         self.config = kwargs.copy()
 
+        self.__paragraph_tool = None
         if "tools" in self.config.keys():
             self.config["tools"] = list(set(self.config["tools"]))
+
+            for tool_idx in range(len(self.config["tools"])):
+                if self.config["tools"][tool_idx].class_name == "Paragraph":
+                    self.__paragraph_tool = self.config["tools"].pop(tool_idx)
+                    break
 
         kwargs.clear()
         super().__init__(*args, **kwargs)
@@ -32,3 +40,17 @@ class EditorJSField(models.JSONField):
     def formfield(self, **kwargs):
         kwargs["widget"] = EditorJSWidget(config=self.config)
         return super().formfield(**kwargs)
+
+    @property
+    def non_db_attrs(self):
+        return super().non_db_attrs + ("tools",)
+
+    @cached_property
+    def tools(self):
+        return { tool.name:tool for tool in [self.__get_paragraph_tool(), *self.config["tools"]] }
+
+    def __get_paragraph_tool(self):
+        if not self.__paragraph_tool:
+            self.__paragraph_tool = Tool(name="paragraph", class_name="Paragraph", url="//cdn.jsdelivr.net/npm/@editorjs/paragraph")
+
+        return self.__paragraph_tool
