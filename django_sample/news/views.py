4e4a9e4684b761a2dd0a0d264a79c8a992686e73
Evgeniy Gribanov <evgeniy.therabbit@gmail.com>
2022-12-21 11:33:33 +0300

    merge: Объединил ветки

--- django_sample/news/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import ListView
-
-from .models import Article
-
-
-class ArticlesListView(ListView):
-    model = Article
-    template_name = "articles-list.html"
-    context_object_name = "articles_list"
